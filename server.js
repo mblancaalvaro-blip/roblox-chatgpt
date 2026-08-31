d9d5f4fba6903ec9a401761addfa60ae9adde2ae
mblancaalvaro-blip <mblancaalvaro@gmail.com>
2026-08-31 14:57:27 +0200

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-server.js
-
 const express = require("express");
 const OpenAI = require("openai");
 
 const app = express();
+
 app.use(express.json());
 
 const client = new OpenAI({
@@ -15,13 +14,25 @@ app.post("/chat", async (req, res) => {
     const response = await client.responses.create({
       model: "gpt-5",
       input: req.body.message
-    });s
+    });
 
-    res.json({ reply: response.output_text });
+    res.json({
+      reply: response.output_text
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Error al conectar con OpenAI" });
+    res.status(500).json({
+      error: "Error al conectar con OpenAI"
+    });
   }
 });
 
-app.listen(process.env.PORT || 3000);
+app.get("/", (req, res) => {
+  res.send("Servidor ChatGPT funcionando");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
